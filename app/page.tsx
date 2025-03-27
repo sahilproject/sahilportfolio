95c2a2d1b0277d7f0bc1b24a3b47873deb8c9f83
sahil max <sahilrahaman585@gmail.com>
2025-03-28 01:06:12 +0530

    Updated

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,6 @@
-import { client } from "@/sanity/lib/client";
 import Home from "./Home";
 
 export default async function Page() {
-  const projects = await client.fetch(`*[_type == "project"]`);
 
-  return <Home projects={projects} />;
+  return <Home/>;
 }
